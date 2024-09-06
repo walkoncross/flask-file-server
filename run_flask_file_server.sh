8956681424c733a0f9708b82e1270831fc817885
zhaoyafei <zhaoyafei0210@gmail.com>
2024-09-06 21:00:27 +0300

    support input root dir in run_flask_file_server.sh

--- run_flask_file_server.sh
+++ run_flask_file_server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash 
-python flask_file_server/server.py --root $(pwd) --host 127.0.0.1 --port 8080
+
+if [ $# -gt 0 ]; then
+    root=$1
+else
+    root=$(pwd)
+fi
+
+python flask_file_server/server.py --root $root --host 127.0.0.1 --port 8080
